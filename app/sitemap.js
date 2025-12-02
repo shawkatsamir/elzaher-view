6556d645e4a856933be760da405dc8ba7ca01bb6
Shawket Ssamir <shawketsamir365@gmail.com>
2025-12-02 03:54:26 +0200

    feat:add sitemap.js and robots.ts files

---
+++ app/sitemap.js
@@ -0,0 +1,43 @@
+import { client } from "../sanity/client";
+
+export default async function sitemap() {
+  const baseUrl = "https://alzaherview.com";
+
+  const postsQuery = `*[_type == "post"] {
+    "slug": slug.current,
+    "category": categories[0]->slug.current,
+    _updatedAt
+  }`;
+  const posts = await client.fetch(postsQuery);
+
+  const categoriesQuery = `*[_type == "category"] {
+    "slug": slug.current,
+    _updatedAt
+  }`;
+  const categories = await client.fetch(categoriesQuery);
+
+  const staticRoutes = ["", "/blog", "/about", "/contact", "/services"].map(
+    (route) => ({
+      url: `${baseUrl}${route}`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: "monthly",
+      priority: route === "" ? 1 : 0.8,
+    }),
+  );
+
+  const categoryRoutes = categories.map((category) => ({
+    url: `${baseUrl}/blog/${category.slug}`,
+    lastModified: category._updatedAt,
+    changeFrequency: "weekly",
+    priority: 0.8,
+  }));
+
+  const postRoutes = posts.map((post) => ({
+    url: `${baseUrl}/blog/${post.category}/${post.slug}`,
+    lastModified: post._updatedAt,
+    changeFrequency: "weekly",
+    priority: 0.7,
+  }));
+
+  return [...staticRoutes, ...categoryRoutes, ...postRoutes];
+}
